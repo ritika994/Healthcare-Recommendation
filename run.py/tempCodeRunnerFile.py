d1fcef81263321400a8f0a104460d1244e9a2f38
ritika994 <ritikajangir39@gmail.com>
2024-04-27 23:30:11 +0530

    Updated

--- run.py/tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier, XGBoostClassifier
-from sklearn.preprocessing import LabelEncoder
-from sklearn.linear_model import LinearRegression  # For regression model
